d0d2d2b525fe1dcec673453407f380e65bba5cc2
Tiago Luchini <info@tiagoluchini.eu>
2009-05-12 08:54:41 +0300

    Adding a post-receive hook

---
+++ post-receive.php
@@ -0,0 +1,4 @@
+<?php
+system('/home/tiagoluchini/git pull origin master');
+system('/home/tiagoluchini/.gem/ruby/1.8/bin/jekyll /home/tiagoluchini/tiagoluchini-blog/ /home/tiagoluchini/tiagoluchini.eu --no-auto');
+?>
